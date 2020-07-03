04ee6c6f2ce999acffb7d8cb2fdc0cdb59946e01
liujie <572890972@qq.com>
2020-07-03 16:56:32 +0800

    home layout finish

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    我是首页
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -8,7 +17,43 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
-
-}
+  name: "Home",
+};
 </script>
+
+
+<style lang="less" scoped>
+
+
+
+
+
+
+
+.home{
+  height: 100%;
+}
+.el-container{
+  height: 100%;
+}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    height: 60px;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    // height: 100%;
+    // line-height: 200px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    // text-align: center;
+    // line-height: 160px;
+  }
+</style>
